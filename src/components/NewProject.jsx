40548bd614915d960607f747b862dcb7db6167a8
Pluck Yang <pluck.yang@gmail..com>
2024-11-04 15:40:09 +0900

    validating user input & show an error modal

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,10 @@
 import Input from "./Input";
 import { useRef } from "react";
+import Modal from "./Modal.jsx";
 
 export default function NewProject({onAdd}) {
+  const modal = useRef();
+  
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -11,7 +14,14 @@ export default function NewProject({onAdd}) {
     const enteredDescription = description.current.value;
     const enteredDueDate = dueDate.current.value;
 
-    // validation ...
+    if(
+      enteredTitle.trim() === '' || 
+      enteredDescription.trim()==='' || 
+      enteredDueDate.trim()===''
+    ){
+      modal.current.open();
+      return;
+    }
 
     onAdd({
       title: enteredTitle,
@@ -21,6 +31,12 @@ export default function NewProject({onAdd}) {
   }
 
   return (
+    <>
+    <Modal ref={modal} buttonCaption="Okay">
+      <h2>Invalid Input</h2>
+      <p>Oops... looks like you forgot to enter a value.</p>
+      <p>Please make sure you provide a valid value for every input field.</p>
+    </Modal>
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
         <li>
@@ -43,5 +59,6 @@ export default function NewProject({onAdd}) {
         <Input type="date" ref={dueDate} label="Due Date" />
       </div>
     </div>
+    </>
   );
 }
--- src/components/ProjectsSideBar.jsx
+++ src/components/ProjectsSideBar.jsx
@@ -12,7 +12,8 @@ export default function ProjectsSideBar({onStartAddProject, projects}){
     <ul className="mt-8">
       {projects.map((project) => (
         <li key={project.id}>
-          <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:text-stone-800">
+          <button 
+            className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">
             {project.title}
           </button>
         </li> 
