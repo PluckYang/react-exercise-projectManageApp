5181f5fa32f238faccdd9b5c0595ed70c1dc89ba
Pluck Yang <pluck.yang@gmail..com>
2024-11-07 09:11:27 +0900

    handling project deletion

--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,6 +1,6 @@
 import { data } from "autoprefixer";
 
-export default function SelectedProject({ project }) {
+export default function SelectedProject({ project, onDelete }) {
 
   const formattedDate = new Date(project.dueDate).toLocaleDateString('en-US', {
     year: 'numeric',
@@ -15,7 +15,10 @@ export default function SelectedProject({ project }) {
           <h1 className="text-3xl font-bold text-stone-600 mb-2">
             {project.title}
           </h1>
-          <button className="text-stone-600 hover:text-stone-950">
+          <button 
+            className="text-stone-600 hover:text-stone-950"
+            onClick={onDelete}
+          >
             Delete
           </button>
         </div>
